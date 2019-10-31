88111507602c52b6b83d2ccc478e2622f2e7f789
jhon.magdalena <jhon.magdalena@imatia.com>
2019-10-31 12:24:22 +0100

    androidx

--- materialwheelview/src/main/java/io/blackbox_vision/wheelview/view/WheelView.java
+++ materialwheelview/src/main/java/io/blackbox_vision/wheelview/view/WheelView.java
@@ -9,13 +9,14 @@ import android.graphics.Rect;
 import android.graphics.Typeface;
 import android.os.Build;
 import android.os.Handler;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 import java.util.ArrayList;
 import java.util.Dictionary;
